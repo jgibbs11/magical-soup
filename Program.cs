a0be71d9f9ed88446a92c1c54e767aaea759fb84
jgibbs11 <73393188+jgibbs11@users.noreply.github.com>
2020-11-01 14:29:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+
+            userName = "John";
+
+            string des1;
+
+                des1 = "Mexico";
+
+            string des2;
+
+                des2 = "Jamaica";
+
+            Console.WriteLine("Thank you for choosing this application please enter you user name " + userName + ".");
+
+            Console.WriteLine("Welcome to Your Vacation Budget Planner " + userName + "!");
+
+            Console.WriteLine("Would you like to go to " + des1 + " or " + des2 + "?");
+
+            Console.ReadLine();
+
+        }
+    }
+}
